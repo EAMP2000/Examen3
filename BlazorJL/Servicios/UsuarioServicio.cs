5270644800c07e4ee5739069ed4c7e216909eb4a
EAMP2000 <98430523+EAMP2000@users.noreply.github.com>
2022-04-26 01:11:50 -0600

    26 Abril 2022
    Examen3

---
+++ BlazorJL/Servicios/UsuarioServicio.cs
@@ -0,0 +1,33 @@
+﻿using BlazorJL.Data;
+using Datos.Interfaces;
+using Datos.Repositorios;
+using BlazorJL.Interfaces;
+using Modelos;
+
+namespace Examen3EP.Servicios;
+
+public class UsuarioServicio : IUsuarioServicio
+{
+    private readonly MySQLConfiguration _configuration;
+    private IUsuarioRepositorio usuarioRepositorio;
+
+    public UsuarioServicio(MySQLConfiguration configuration)
+    {
+        _configuration = configuration;
+        usuarioRepositorio = new UsuarioRepositorio(configuration.CadenaConexion);
+    }
+
+    public async Task<IEnumerable<Usuario>> GetLista()
+    {
+        return await usuarioRepositorio.GetLista();
+    }
+
+    public async Task<Usuario> GetPorCodigo(string codigo)
+    {
+        return await usuarioRepositorio.GetPorCodigo(codigo);
+    }
+
+
+
+
+}
